2ceb113e98dee323f18fedb496e7e5ba08577cea
smitkhandar <bca2022smit1695@tnraocollege.org>
2024-08-12 10:04:34 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,16 @@
+emp = {
+    'id': 1,
+    'name': 'smit',
+    'designation': 'ceo',
+    'salary': 600000
+}
+
+print("Dictionary:",emp)
+
+# Delete the 'designation' key
+del emp['designation']
+print("\nDeleting Designation:",emp)
+
+# Update the 'name' field
+emp['name'] = 'smit khandar'
+print("\nUpdating 'name':",emp)
